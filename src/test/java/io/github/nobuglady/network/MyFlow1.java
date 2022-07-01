8306cdeb6dc57d93dfe599b03b11ce9c89e8b33a
nobuglady <nobuglady@protonmail.com>
2022-07-01 16:23:40 +0900

    create

---
+++ src/test/java/io/github/nobuglady/network/MyFlow1.java
@@ -0,0 +1,28 @@
+package io.github.nobuglady.network;
+
+import io.github.nobuglady.network.fw.FlowRunner;
+import io.github.nobuglady.network.fw.annotation.Node;
+
+public class MyFlow1 extends FlowRunner {
+
+	@Node(label="a")
+	public void process_a() {
+		System.out.println("processing a");
+	}
+	
+	@Node(label="b")
+	public void process_b() {
+		System.out.println("processing b");
+		throw new RuntimeException("test");
+	}
+	
+	@Node(label="c")
+	public void process_c() {
+		System.out.println("processing c");
+	}
+
+	@Node(label="d")
+	public void process_d() {
+		System.out.println("processing d");
+	}
+}
---
+++ src/test/java/io/github/nobuglady/network/Test.java
@@ -0,0 +1,18 @@
+package io.github.nobuglady.network;
+
+import io.github.nobuglady.network.fw.starter.FlowStarter;
+
+public class Test {
+
+	public static void main(String[] args) {
+		MyFlow1 myFlow1 = new MyFlow1();
+		myFlow1.startFlow();
+		
+		try {
+			Thread.sleep(10000);
+			FlowStarter.shutdown();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
